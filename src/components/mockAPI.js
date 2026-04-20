9ee8eb67691b92e8501335beddcf735c48b80f4c
Menna Elsba3i <elsbaayemenna@gmail.com>
2026-04-20 23:00:18 +0200

    Update Support questions, responsive the nav bar in landing and fixing the navigate  logo in auth

--- src/components/mockAPI.js
+++ src/components/mockAPI.js
@@ -1,8 +1,7 @@
 // const API_BASE_URL = 'https://nontelepathically-pamphletary-cyndi.ngrok-free.dev';
 // const API_BASE_URL = 'https://toothlike-intermetatarsal-avah.ngrok-free.dev';
 // const API_BASE_URL = 'https://unpersecuted-vanitied-jayson.ngrok-free.dev';
-// const API_BASE_URL = 'https://unallegedly-wrinkly-claribel.ngrok-free.dev/';
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://unallegedly-wrinkly-claribel.ngrok-free.dev';
 
 
 import { getCookie, setCookie, deleteCookie, setJsonCookie } from './cookieUtils';
